4eccd2838e2e9994788841708f5faf02475e9b5e
K-Jacko <wakumx@icloud.com>
2023-05-26 21:36:23 +0100

    Components

--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom/client';
 
 function Greeting()
 {
-    return React.createElement('div', {}, React.createElement('h2', {}, 'Hi World, Im in a div'))
-};
+    return (
+        <div>
+            <Person/>
+            <Message/>
+        </div>
+    );
+}
+
+    const Person = () => <h2>John Doe</h2>;
+    const Message = () => <p>This is my Message</p>;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(<Greeting></Greeting>);
